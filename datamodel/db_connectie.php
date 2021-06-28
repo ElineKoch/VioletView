996a3e7b6913e2f772e27b6d650df3e903a69f7a
Eline Koch <eline.koch@hotmail.com>
2021-06-28 22:35:16 +0200

    add login file

--- datamodel/db_connectie.php
+++ datamodel/db_connectie.php
@@ -1,15 +1,19 @@
 <?php
 
-$host = 'localhost';
-$db = 'fletnix_db_rh';
-$user = 'Eline';
-$pass = 'testwachtwoord';
+$login = explode("\n", file_get_contents('datamodel/files/login.txt'));
+$arrayLength = count($login);
+
+for($i = 0; $i < $arrayLength; $i++) {
+    [$x, $y] = explode(' = ', trim($login[$i], " \n\r\t\v\0"));
+    $login[$x] = $y;
+    unset($login[$i]);
+} 
 
 try {
     $dbh = new PDO(
-        "mysql:host=$host;dbname=$db;ConnectionPooling=0",
-        $user,
-        $pass
+        "mysql:host={$login['host']};dbname={$login['db']};ConnectionPooling=0",
+        $login['user'],
+        $login['pass']
     );
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
